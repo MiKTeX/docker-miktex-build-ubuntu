168259905d6221cb1f720d43389f90344d2b373f
Christian Schenk <cs@miktex.org>
2018-02-13 23:24:28 +0100

    Fix UID issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-USER_ID=${USER_ID:-1001}
 GROUP_ID=${GROUP_ID:-1001}
+USER_ID=${USER_ID:-1001}
 
+groupadd -g $GROUP_ID -o joe
 useradd --shell /bin/bash -u $USER_ID -g $GROUP_ID -o -c "" -m joe
 export HOME=/home/joe
 exec /usr/local/bin/gosu joe "$@"
